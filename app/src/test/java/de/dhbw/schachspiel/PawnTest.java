3b83f7e5c96d60cf40f659b8bfb2fc88e5a2d8da
Jannis Fuchs <janifu02@gmail>
2025-05-04 14:05:47 +0200

    professionalised naming
    Signed-off-by: Jannis Fuchs <janifu02@gmail>

--- app/src/test/java/de/dhbw/schachspiel/PawnTest.java
+++ app/src/test/java/de/dhbw/schachspiel/PawnTest.java
@@ -12,77 +12,77 @@ import java.util.Map;
 class PawnTest
 {
 
-	@Test
-	void capture() throws Move.IllegalMoveException
-	{
-		Move move = new Move("exd5", PieceColor.WHITE);
-		Map<Field, IPiece> importantPieces = Map.of(
-				new Field(4, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e4
-				new Field(3, 3), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.BLACK), //d5
-				new Field(4, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //c4
-				new Field(4, 3), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //d4
-		);
-		IBoard board = new MockBoard(importantPieces);
-		FieldSet possibleFields = new FieldSet();
-		possibleFields.add(new Field(4, 2));
-		possibleFields.add(new Field(4, 4));
-		Field f = move.piece.calculateStartField(possibleFields, move, board);
-		Assertions.assertEquals(new Field(4, 4), f);
-	}
+    @Test
+    void capture() throws Move.IllegalMoveException
+    {
+        Move move = new Move("exd5", PieceColor.WHITE);
+        Map<Field, IPiece> importantPieces = Map.of(
+                new Field(4, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e4
+                new Field(3, 3), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.BLACK), //d5
+                new Field(4, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //c4
+                new Field(4, 3), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //d4
+        );
+        IBoard board = new MockBoard(importantPieces);
+        FieldSet possibleFields = new FieldSet();
+        possibleFields.add(new Field(4, 2));
+        possibleFields.add(new Field(4, 4));
+        Field calculatedStartField = move.piece.calculateStartField(possibleFields, move, board);
+        Assertions.assertEquals(new Field(4, 4), calculatedStartField);
+    }
 
-	@Test
-	void move2Squares() throws Move.IllegalMoveException
-	{
-		Move move = new Move("e4", PieceColor.WHITE);
-		Map<Field, IPiece> importantPieces = Map.of(
-				new Field(6, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e2
-				new Field(6, 3), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //d2
-				new Field(6, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //f2
-		);
-		IBoard board = new MockBoard(importantPieces);
-		FieldSet possibleFields = new FieldSet();
-		possibleFields.add(new Field(6, 4));
-		possibleFields.add(new Field(6, 3));
-		possibleFields.add(new Field(6, 2));
-		Field f = move.piece.calculateStartField(possibleFields, move, board);
-		Assertions.assertEquals(new Field(6, 4), f);
-	}
+    @Test
+    void move2Squares() throws Move.IllegalMoveException
+    {
+        Move move = new Move("e4", PieceColor.WHITE);
+        Map<Field, IPiece> importantPieces = Map.of(
+                new Field(6, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e2
+                new Field(6, 3), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //d2
+                new Field(6, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //f2
+        );
+        IBoard board = new MockBoard(importantPieces);
+        FieldSet possibleFields = new FieldSet();
+        possibleFields.add(new Field(6, 4));
+        possibleFields.add(new Field(6, 3));
+        possibleFields.add(new Field(6, 2));
+        Field calculatedStartField = move.piece.calculateStartField(possibleFields, move, board);
+        Assertions.assertEquals(new Field(6, 4), calculatedStartField);
+    }
 
-	@Test
-	void moveBlocked() throws Move.IllegalMoveException
-	{
-		Move move = new Move("e4", PieceColor.WHITE);
-		Map<Field, IPiece> importantPieces = Map.of(
-				new Field(6, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e2
-				new Field(5, 4), PieceFactory.createPieceFromType(PieceType.ROOK, PieceColor.WHITE), //e3
-				new Field(6, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //f4
-		);
-		IBoard board = new MockBoard(importantPieces);
-		FieldSet possibleFields = new FieldSet();
-		possibleFields.add(new Field(6, 4));
-		possibleFields.add(new Field(6, 3));
-		possibleFields.add(new Field(6, 2));
-		Assertions.assertThrows(Move.IllegalMoveException.class, () -> move.piece.calculateStartField(possibleFields, move, board));
-	}
+    @Test
+    void moveBlocked() throws Move.IllegalMoveException
+    {
+        Move move = new Move("e4", PieceColor.WHITE);
+        Map<Field, IPiece> importantPieces = Map.of(
+                new Field(6, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e2
+                new Field(5, 4), PieceFactory.createPieceFromType(PieceType.ROOK, PieceColor.WHITE), //e3
+                new Field(6, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //f4
+        );
+        IBoard board = new MockBoard(importantPieces);
+        FieldSet possibleFields = new FieldSet();
+        possibleFields.add(new Field(6, 4));
+        possibleFields.add(new Field(6, 3));
+        possibleFields.add(new Field(6, 2));
+        Assertions.assertThrows(Move.IllegalMoveException.class, () -> move.piece.calculateStartField(possibleFields, move, board));
+    }
 
-	@Test
-	void moveOneSquare() throws Move.IllegalMoveException
-	{
-		Move move = new Move("e4", PieceColor.WHITE);
-		Map<Field, IPiece> importantPieces = Map.of(
-				new Field(6, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e2
-				new Field(5, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e3
-				new Field(6, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //f4
-		);
-		IBoard board = new MockBoard(importantPieces);
-		FieldSet possibleFields = new FieldSet();
-		possibleFields.add(new Field(6, 4));
+    @Test
+    void moveOneSquare() throws Move.IllegalMoveException
+    {
+        Move move = new Move("e4", PieceColor.WHITE);
+        Map<Field, IPiece> importantPieces = Map.of(
+                new Field(6, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e2
+                new Field(5, 4), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE), //e3
+                new Field(6, 2), PieceFactory.createPieceFromType(PieceType.PAWN, PieceColor.WHITE) //f4
+        );
+        IBoard board = new MockBoard(importantPieces);
+        FieldSet possibleFields = new FieldSet();
+        possibleFields.add(new Field(6, 4));
 
-		possibleFields.add(new Field(5, 4));
-		possibleFields.add(new Field(6, 3));
-		possibleFields.add(new Field(6, 2));
-		Field f = move.piece.calculateStartField(possibleFields, move, board);
-		Assertions.assertEquals(new Field(5, 4), f);
-	}
+        possibleFields.add(new Field(5, 4));
+        possibleFields.add(new Field(6, 3));
+        possibleFields.add(new Field(6, 2));
+        Field calculatedStartField = move.piece.calculateStartField(possibleFields, move, board);
+        Assertions.assertEquals(new Field(5, 4), calculatedStartField);
+    }
 
 }
